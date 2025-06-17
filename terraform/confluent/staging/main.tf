b1baa19245ead62d2e54368249dc4babcab9e1be
dammyadeniji12 <dami.adeniji12@gmail.com>
2025-06-17 22:57:50 +0100

    created app_consumer service account

--- terraform/confluent/staging/main.tf
+++ terraform/confluent/staging/main.tf
@@ -202,3 +202,80 @@ resource "confluent_api_key" "app-producer-kafka-api-key" {
     confluent_service_account.app-producer
   ]
 }
+
+
+
+
+resource "confluent_service_account" "app-consumer" {
+  display_name = "app-consumer"
+  description  = "Service account to read from 'customer-information' topic of 'staging' Kafka cluster"
+}
+
+
+resource "confluent_kafka_acl" "app-consumer-read-on-topic" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+  resource_type = "TOPIC"
+  resource_name = confluent_kafka_topic.customer-information.topic_name
+  pattern_type  = "LITERAL"
+  principal     = "User:${confluent_service_account.app-consumer.id}"
+  host          = "*"
+  operation     = "READ"
+  permission    = "ALLOW"
+  rest_endpoint = confluent_kafka_cluster.basic.rest_endpoint
+  credentials {
+    key    = confluent_api_key.app-manager-kafka-api-key.id
+    secret = confluent_api_key.app-manager-kafka-api-key.secret
+  }
+  depends_on = [
+    confluent_service_account.app-consumer
+  ]
+}
+
+resource "confluent_kafka_acl" "app-consumer-read-on-topic-consumer-group" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+  resource_type = "GROUP"
+  resource_name = "DLA-"
+  pattern_type  = "PREFIXED"
+  principal     = "User:${confluent_service_account.app-consumer.id}"
+  host          = "*"
+  operation     = "READ"
+  permission    = "ALLOW"
+  rest_endpoint = confluent_kafka_cluster.basic.rest_endpoint
+  credentials {
+    key    = confluent_api_key.app-manager-kafka-api-key.id
+    secret = confluent_api_key.app-manager-kafka-api-key.secret
+  }
+  depends_on = [
+    confluent_service_account.app-consumer
+  ]
+}
+
+
+resource "confluent_api_key" "app-consumer-kafka-api-key" {
+  display_name = "app-consumer-kafka-api-key"
+  description  = "Kafka API Key that is owned by 'app-consumer' service account"
+  owner {
+    id          = confluent_service_account.app-consumer.id
+    api_version = confluent_service_account.app-consumer.api_version
+    kind        = confluent_service_account.app-consumer.kind
+  }
+
+  managed_resource {
+    id          = confluent_kafka_cluster.basic.id
+    api_version = confluent_kafka_cluster.basic.api_version
+    kind        = confluent_kafka_cluster.basic.kind
+
+    environment {
+      id = confluent_environment.staging.id
+    }
+  }
+
+  depends_on = [
+    confluent_service_account.app-consumer,
+    confluent_kafka_acl.app-consumer-read-on-topic
+  ]
+}
--- terraform/confluent/staging/output.tf
+++ terraform/confluent/staging/output.tf
@@ -17,3 +17,14 @@ output "app_producer_schema_registry_secret" {
   value     = confluent_api_key.schema_registry_key.secret
   sensitive = true
 }
+
+
+output "app_consumer_api_key" {
+  value     = confluent_api_key.app-consumer-kafka-api-key.id
+  sensitive = true
+}
+
+output "app_consumer_api_secret" {
+  value     = confluent_api_key.app-consumer-kafka-api-key.secret
+  sensitive = true
+}
